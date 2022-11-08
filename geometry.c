885b123ab796e03c3cb321acfd98acaf877eec8f
DENOCE Allan <Allan.Denoce@bordeaux-inp.fr>
2022-11-08 17:16:34 +0100

    Modifiation du geometry.c

--- geometry.c
+++ geometry.c
@@ -1,11 +1,34 @@
 //
-#include geometry.h
+#include <stdio.h>
+#include <string.h>
+#include "./src/geometry.h"
 
 const char* place_to_string(enum color_t c, enum sort_t s){
-
+  char* color;
+  char* sort;
+  if(c == 0){
+    color = "NO_COLOR";
+  }
+  if(c == 1){
+    color = "BLACK";
+  }
+  if(c == 2){
+    color = "WHITE";
+  }
+  else{
+    return "INVALID_COLOR";
+  }
+  if(s == 0){
+    sort = "NO_SORT";
+  }
+  if(s == 1){
+    sort = "PAWN";
+  }
+  else{
+    return "INVALID_SORT";
+  }
+  return strcat(color, sort);
 }
 
 
-const char* dir_to_string(enum dir_t d){
 
-}
