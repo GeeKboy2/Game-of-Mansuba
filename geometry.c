109ee94fbacbb09dde41fa0824113c57b7f4d4d3
KHERRAZ Mohamed <Mohamed.Kherraz@bordeaux-inp.fr>
2022-11-08 17:41:22 +0100

    Ecricture dir_to_string

--- geometry.c
+++ geometry.c
@@ -1,11 +1,55 @@
 //
-#include geometry.h
+#include <stdio.h>
+#include "geometry.h"
 
 const char* place_to_string(enum color_t c, enum sort_t s){
 
 }
 
 
-const char* dir_to_string(enum dir_t d){
-
+const char* dir_to_string(enum dir_t d)
+{
+  char* str;
+  printf("%d",d);
+  if (d == 0)
+  {
+    str = "NO_DIR";
+  }
+  if (d == 1)
+  {
+    str = "EAST";
+  }
+  if (d == 2)
+  {
+    str = "NEAST";
+  }
+  if (d == 3)
+  {
+    str = "NORTH";
+  }
+  if (d == 4)
+  {
+    str = "NWEST";
+  }
+    if (d == -1)
+  {
+    str = "WEST";
+  }
+    if (d == -2)
+  {
+    str = "SWEST";
+  }
+  if (d == -3)
+  {
+    str = "SOUTH";
+  }
+    if (d == -4)
+  {
+    str = "SEAST";
+  }
+    if (d>4 || d<-4)
+  {
+    str = "INVALID DIRECTION";
+  }
+  return str;
 }
