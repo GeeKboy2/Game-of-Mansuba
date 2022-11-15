3fbe767179bdaa21bf0261f3de6c526426c1dda1
DENOCE Allan <Allan.Denoce@bordeaux-inp.fr>
2022-11-15 17:04:19 +0100

    Création fonction position_init dans rules.c

--- rules.c
+++ rules.c
@@ -6,28 +6,28 @@
 
 // Règle de base : Noir à gauche, Blanc à droite
 void position_init(struct world_t* b){
-  for(int i; i < WORLD_SIZE; i++){
+  enum color_t color;
+  enum sort_t sort=1;
+  for(int i = 0; i < WORLD_SIZE; i++){
     if(i%WIDTH == 0){
-      enum color_t color=1;
-      world_set(b,i,color);
-      enum sort_t sort=1;
+      color=1;
+      world_set(b,i,color);      
       world_set_sort(b,i,sort);
     }
     if(i%WIDTH == WIDTH - 1){
-      enum color_t color=2;
+      color=2;
       world_set(b,i,color);
-      enum sort_t sort=1;
       world_set_sort(b,i,sort);
     }
   }
 }
 
-int main(int argc, char *argv[]){
+void test_position_init(){
   struct world_t* b;
   b = world_init();
   position_init(b);
   for(int i; i<WORLD_SIZE;i++){
-    printf("i : %d, %d, %d\n",i, p.point[i].c,p.point[i].s); 
+    printf("i : %d --> %d, %d\n",i, b->point[i].c,b->point[i].s); 
   }
 }
 
