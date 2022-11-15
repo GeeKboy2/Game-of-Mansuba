7cec1c2617a014de637f73f36206ee9dd049eb81
DENOCE Allan <Allan.Denoce@bordeaux-inp.fr>
2022-11-15 17:51:33 +0100

    Modification de rules.c

--- rules.c
+++ rules.c
@@ -4,6 +4,36 @@
 #include "./src/world.h"
 #include "world.c"
 
+struct piece{
+  int blanc[HEIGHT];
+  int noir[HEIGHT];
+};
+
+struct piece piece;
+void position_init(){
+  int b = 0;
+  int n = 0;
+  for(int i = 0; i< WORLD_SIZE; i++){
+    if(i%WIDTH==0){
+      piece.noir[n]=i;
+      n++;
+    }
+    if(i%WIDTH==WIDTH-1){
+      piece.blanc[b]=i;
+      b++;
+    }
+  }
+}
+
+int main(int argc,char *argv[]){
+  position_init();
+  for(int i = 0;i<HEIGHT;i++){
+    printf("BLANC[%d] = %d | NOIR[%d] = %d\n",i,piece.blanc[i],i,piece.noir[i]);
+  }
+  return 0;
+}
+
+/*
 // Règle de base : Noir à gauche, Blanc à droite
 void position_init(struct world_t* b){
   enum color_t color;
@@ -31,4 +61,4 @@ void test_position_init(){
   }
 }
 
-
+*/
