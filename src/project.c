87c1f29973766a43f6093312f528ce532c40314c
KHERRAZ Mohamed <Mohamed.Kherraz@bordeaux-inp.fr>
2022-12-02 14:42:36 +0100

    second try at compiling with neighbors.h

--- src/project.c
+++ src/project.c
@@ -19,6 +19,12 @@ int position_init(struct world_t* world);
 
 int condition_victoire(struct world_t * world,char *type_victoire,int MAX_TURNS,int TURN);
 
+struct neighbors_t deplacement_simple(struct world_t* world, unsigned int idx);
+
+struct neighbors_t saut_simple(struct world_t* world, unsigned int idx);
+
+unsigned int nombre_mouvements(struct world_t* world, unsigned int idx);
+
 void show_world(struct world_t* world)
 {
   for(int i = 0; i <WORLD_SIZE ; i++){
