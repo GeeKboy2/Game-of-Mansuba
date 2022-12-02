113f962ec1ea89982eeff25d752b6e3a91ba9ef7
KHERRAZ Mohamed <Mohamed.Kherraz@bordeaux-inp.fr>
2022-12-02 15:01:51 +0100

    suppression de signature dans le project.c

--- src/project.c
+++ src/project.c
@@ -15,16 +15,6 @@
 //#define TURN 0
 //#define UINT_MAX 0
 
-int position_init(struct world_t* world);
-
-int condition_victoire(struct world_t * world,char *type_victoire,int MAX_TURNS,int TURN);
-
-struct neighbors_t deplacement_simple(struct world_t* world, unsigned int idx);
-
-struct neighbors_t saut_simple(struct world_t* world, unsigned int idx);
-
-unsigned int nombre_mouvements(struct world_t* world, unsigned int idx);
-
 void show_world(struct world_t* world)
 {
   for(int i = 0; i <WORLD_SIZE ; i++){
@@ -45,7 +35,6 @@ void show_world(struct world_t* world)
     }    
   }
 }
-
 
 enum color_t get_random_player()
 {
